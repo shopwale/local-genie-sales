8c357fa2f25a6fbc22cfa54f61c5b00aee04ead8
Ahamed <ahmedtamseer3@gmail.com>
2022-10-01 17:49:34 +0530

    notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotificationComponent,
   ],
   imports: [
     BrowserModule,
